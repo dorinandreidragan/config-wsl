98e2995727abeb47f9e1bc1d04277ee97dbf89df
Dorin Andrei Dragan <dorin.andrei.dragan@gmail.com>
2022-01-14 11:15:45 +0200

    Move config.sh to install-podman.sh

--- install-podman.sh
+++ install-podman.sh
@@ -1,5 +1,5 @@
 # https://www.redhat.com/sysadmin/podman-windows-wsl2
-# install podman for wsl2
+# Install podman for wsl2
 . /etc/os-release
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/x${NAME}_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
 wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/x${NAME}_${VERSION_ID}/Release.key -O Release.key
